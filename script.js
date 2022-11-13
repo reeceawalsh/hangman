5f914a4f2f492c8b3105e39a3ff450caf838f2e1
ryhhill1998 <ryhhill1998@outlook.com>
2022-11-13 11:36:17 +0000

    Added function outlines and words list to script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// -------------------- GLOBAL VARIABLES -------------------- //
+
+// temperary words list for testing before implementing API solution
+const wordsList = [
+  "The Shawshank Redemption",
+  "The Godfather",
+  "The Dark Knight",
+  "The Godfather: Part II",
+  "12 Angry Men",
+  "Schindler's List",
+  "The Lord of the Rings: The Return of the King",
+  "Pulp Fiction",
+  "The Lord of the Rings: The Fellowship of the Ring",
+  "The Good, the Bad and the Ugly",
+];
+
+// -------------------- FUNCTIONS -------------------- //
+
+// 1. generate chosen word from words list
+
+// 2. get user input
+
+// 3. check whether guessed letter is in chosen word
+
+// 4. reduce lives
+
+// 5. check if game is over (if player has won or is out of lives)
+
+// 6. reset game if player wants to play again
